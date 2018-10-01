38f297dad521e7aa3c22bbb8b5bc2c9c0570fa96
Matt Scorca <mscorca@ucsc.edu>
2018-10-01 13:38:58 -0700

    project

---
+++ Assets/Scripts/LyricMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LyricMover : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	void Update () 
+    {
+        switch(SoundManager.Instance.SongIndex) 
+        {
+//            case 0:
+
+        }
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour
+{
+
+    private static SoundManager _instance;
+
+    public static SoundManager Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public int SongIndex;
+    public List<AudioClip> Songs;
+    public AudioSource Player;
+
+    private void Start()
+    {
+        Player = GetComponent<AudioSource>();
+    }
+
+    protected void Update()
+    {
+        if(!Player.isPlaying) 
+        {
+            Player.clip = Songs[SongIndex];
+            Player.Play();
+            SongIndex++;
+        }
+    }
+
+}
